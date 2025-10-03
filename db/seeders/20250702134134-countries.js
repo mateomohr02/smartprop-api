d518c1e3050d8ecf2beb0a86e6fd901375d1624c
Mateo Mohr <mohr.mateo@gmail.com>
2025-10-02 22:33:19 -0300

    fix seed

--- db/seeders/20250702134134-countries.js
+++ db/seeders/20250702134134-countries.js
@@ -7,12 +7,12 @@ module.exports = {
       {
         id: "e2712aa9-6e16-4062-a79a-29b47efcf03b",
         name: "Argentina",
-        slug: "-argentina",
+        slug: "argentina",
       },
       {
         id: "7186e3a1-5ed2-4efc-a2ba-da1bafee4194",
         name: "Brasil",
-        slug: "-brasil",
+        slug: "brasil",
       },
     ]);
   },
